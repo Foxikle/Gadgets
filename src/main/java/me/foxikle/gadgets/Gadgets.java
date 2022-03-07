b2d5b0c5895380ce2771017441a8e7a83276ab64
BuildTools <unconfigured@null.spigotmc.org>
2022-03-07 16:23:10 -0500

    Initial commit

---
+++ src/main/java/me/foxikle/gadgets/Gadgets.java
@@ -0,0 +1,17 @@
+package me.foxikle.gadgets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gadgets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
