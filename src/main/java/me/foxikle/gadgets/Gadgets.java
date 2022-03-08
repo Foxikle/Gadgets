9d3539217d8f1ecb5ff256987c6793db30cff156
Foxikle <tristantroop260@gmail.com>
2022-03-07 21:28:43 -0500

    Added an instance getter

--- src/main/java/me/foxikle/gadgets/Gadgets.java
+++ src/main/java/me/foxikle/gadgets/Gadgets.java
@@ -4,12 +4,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Gadgets extends JavaPlugin {
 
+    public static Gadgets instance;
+
     @Override
     public void onEnable() {
         this.saveResource("config.yml", false);
+        instance = this;
     }
 
     @Override
     public void onDisable() {
+    }
+    public static Gadgets getInstance(){
+        return instance;
     }
 }
